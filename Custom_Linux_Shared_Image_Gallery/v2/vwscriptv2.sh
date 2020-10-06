cf06ae1ae37f920b6c7af145cd46a6f37641ed1e
Victor Santana <vsantana@microsoft.com>
2020-10-06 16:29:29 -0500

    add profile files and update script

--- Custom_Linux_Shared_Image_Gallery/v2/vwscriptv2.sh
+++ Custom_Linux_Shared_Image_Gallery/v2/vwscriptv2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 # add epel repo & update
 sudo yum install -y epel-release && sudo yum update -y
+
 # Add repo key for az cli
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
@@ -19,34 +20,59 @@ sudo yum install -y azure-cli
 sudo yum install -y singularity
 
 #install python3
-sudo yum install python3
+# sudo yum install python3
 
 #install R
+sudo dnf install epel-release
+sudo dnf config-manager --set-enabled PowerTools
 sudo yum install -y R
 
 #install SAMTools 
 sudo yum install -y samtools
 
-#download azcopy v7 & v10, use v7 installer script to install v10 & add to path
-wget -O azcopy_v7.tar.gz https://aka.ms/downloadazcopylinux64
-mkdir azcopy_v7
-tar -xf azcopy_v7.tar.gz -C azcopy_v7
-wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux
-mkdir azcopy_v10
-tar -xf azcopy_v10.tar.gz -C azcopy_v10
-# this step may fail with updates, the _amd64_10.x.x version path will change
-mv azcopy_v10/azcopy_linux_amd64_10.6.0 azcopy_v10/azcopy
-cp azcopy_v7/install.sh azcopy_v10/install.sh
-cp azcopy_v7/azcopy/azcopy_autocomplete azcopy_v10/azcopy/
-cp azcopy_v7/azcopy/startup azcopy_v10/azcopy/
-sudo ./azcopy_v10/install.sh
+# Install AzCopy
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+sudo cp azcopy_linux_amd64_10.6.0/azcopy /usr/bin/
+sudo chmod 755 /usr/bin/azcopy
+rm -rf downloadazcopy-v10-linux azcopy_linux_amd64_10.6.0/
+
 #tmux
 sudo yum install -y tmux
+
 #java 11
 sudo yum install -y java-11-openjdk
+sudo yum install -y java-1.8.0-openjdk
+
+# Comments
+# /usr/lib/jvm/java-11-openjdk-11.0.8.10-0.el8_2.x86_64/bin/java
+# /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.265.b01-0.el8_2.x86_64/bin/java
+# which java
+# whereis java
+
+# export JAVA_HOME=/opt/jdk1.8.0_20/bin/java
+# export JRE_HOME=/opt/jdk1.8.0_20/jre/bin/java
+# export PATH=$PATH:$HOME/bin:JAVA_HOME:JRE_HOME
+
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.8.10-0.el8_2.x86_64/bin/java
+# export JRE_HOME=/usr/lib/jvm/java-11-openjdk-11.0.8.10-0.el8_2.x86_64/bin/java
+# export PATH=$PATH:/usr/lib/jvm/java-11-openjdk-11.0.8.10-0.el8_2.x86_64/bin
+
+# environment-modules
+sudo yum install environment-modules -y
+# source /etc/profile.d/modules.sh
+mkdir /etc/modulefiles/java
+wget -O /etc/modulefiles/java/11 https://raw.githubusercontent.com/Welasco/ImageBuilder/master/Custom_Linux_Shared_Image_Gallery/v2/8
+wget -O /etc/modulefiles/java/11 https://raw.githubusercontent.com/Welasco/ImageBuilder/master/Custom_Linux_Shared_Image_Gallery/v2/11
+
+
+# module avail
+# module load java/11
+# module unload java/11
+
 
 # Add preview banner to MOTD
-cat >> /etc/motd << EOF
+sudo tee -a /etc/motd > /dev/null <<'EOF'
 *******************************************************
 **            This VM was built for VWS from the:    **
 **      !! AZURE VM IMAGE BUILDER Custom Image !!    **
